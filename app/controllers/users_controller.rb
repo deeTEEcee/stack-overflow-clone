f152eb9fbb17d8af38cf371a70fe80aafe1cf973
deeteecee <d.tchen@ucla.edu>
2015-03-25 16:13:43 +0900

    auto-login after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login(user_params[:username], user_params[:password])
       redirect_to root_url, :notice => "Signed up"
     else
       render :entry
